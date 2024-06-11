845cec3e4ef9eace635fa396b684e32fcb0c6cfd
Stanislav Cherviakov <stchervyakov@gmail.com>
2024-06-11 18:10:25 +0100

    Update errors.rs
    extended the error message a bit

--- src/errors.rs
+++ src/errors.rs
@@ -6,7 +6,7 @@ pub enum RollupError {
     SolanaClientErr(#[from] solana_rpc_client_api::client_error::Error),
     #[error("Merkle tree bytes parsing error: {0}")]
     UnableToParseTreeErr(#[from] std::io::Error),
-    #[error("Unexpected tree depth={0} and max size={1}")]
+    #[error("Unexpected tree depth={0} and max buffer size={1}")]
     UnexpectedTreeSize(u32, u32),
     #[error("Illegal arguments: {0}")]
     IllegalArgumets(String),
